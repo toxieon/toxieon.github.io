8487642adbe01a4775c3df5923cd77f3d7cd9264
Brandon N <71760080+toxieon@users.noreply.github.com>
2026-05-27 19:51:40 +1000

    Delete planner directory

--- planner/config.js
+++
@@ -1,8 +0,0 @@
-window.NEILL_PLANNER_CONFIG = {
-  googleApiKey: "AIzaSyBZ2b4fa-YV89q6Ld_LfGlzbqM798iCMsA",
-  googleClientId: "418369916603-u3pqd7ngq7nuvd032dagjc5apq8ogg2e.apps.googleusercontent.com",
-  driveRootFolderName: "NeillPlanner",
-  scopes: [
-    "https://www.googleapis.com/auth/drive.file"
-  ]
-};
